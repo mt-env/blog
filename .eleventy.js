cf5fc3a53c0fab108879adbd9697b6acbe1e0279
Henry Zheng <henry.zheng727@gmail.com>
2026-05-01 22:44:28 -0600

    code: add blog post theme

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css/");
     eleventyConfig.addWatchTarget("./src/css/");
 
+    eleventyConfig.addFilter("readableDate", (date) =>
+        new Date(date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric", timeZone: "UTC" })
+    );
+
     return {
         dir: {
             input: "src",
